6007db9e2bae07a131002254228280add09b2359
sanyakoliasko-stack <sanya.koliasko@gmail.com>
2026-04-14 14:49:34 +0300

    .

--- 2.c
+++ 2.c
@@ -1,48 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int main() {
-    int n, m, i, j;
-    printf("Введіть кількість рядків (n): ");
-    scanf("%d", &n);
-    printf("Введіть кількість стовпців (m): ");
-    scanf("%d", &m);
-    double A[n][m];
-    srand(time(NULL));
+    int rows, cols;
+    srand(time(NULL)); // Ініціалізація генератора випадкових чисел
+
+    // 1. Введення розмірів матриці
+    printf("Введіть кількість рядків: ");
+    scanf("%d", &rows);
+    printf("Введіть кількість стовпців: ");
+    scanf("%d", &cols);
+
+    double A[rows][cols]; // Оголошення матриці дійсних чисел
+
+    // 2. Заповнення матриці випадковими числами від -100 до 100
     printf("\nПочаткова матриця:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            // Генеруємо дійсні числа від -100 до 100
-            A[i][j] = (rand() % 20001 - 10000) / 100.0;
-            printf("%8.2f ", A[i][j]);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            // Генеруємо дійсне число: (випадкове число) / (макс_рандом) * діапазон + мінімум
+            A[i][j] = ((double)rand() / RAND_MAX) * 200.0 - 100.0;
+            printf("%8.2f ", A[i][j]); // Вивід з точністю 2 знаки
         }
         printf("\n");
     }
+
+    // 3. Пошук стовпця з найбільшою сумою
     double maxSum;
-    int maxIdx = 0;
-    for (j = 0; j < m; j++) {
+    int maxColIndex = 0;
+
+    for (int j = 0; j < cols; j++) { // Йдемо по стовпцях
         double currentSum = 0;
-        for (i = 0; i < n; i++) {
+        for (int i = 0; i < rows; i++) { // Додаємо всі елементи у стовпці
             currentSum += A[i][j];
         }
-        printf("Сума стовпця %d: %.2f\n", j, currentSum);
+
+        // Якщо це перший стовпець або його сума більша за попередню максимальну
         if (j == 0 || currentSum > maxSum) {
             maxSum = currentSum;
-            maxIdx = j;
+            maxColIndex = j;
         }
     }
-    printf("\nВилучаємо стовпець №%d (сума: %.2f)\n", maxIdx, maxSum);
-    for (i = 0; i < n; i++) {
-        for (j = maxIdx; j < m - 1; j++) {
-            A[i][j] = A[i][j + 1];
-        }
-    }
+
+    printf("\nСтовпець для видалення: %d (Сума: %.2f)\n", maxColIndex, maxSum);
+
+    // 4. Виведення матриці БЕЗ цього стовпця (фактичне вилучення при виводі)
     printf("\nМатриця після вилучення стовпця:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m - 1; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (j == maxColIndex) continue; // Пропускаємо стовпець, який треба видалити
             printf("%8.2f ", A[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
